6f8f2ae9574874258c84525277176d15eb319ae1
Sanjay Kumar <sanjayvkumar31@gmail.com>
2025-03-16 17:06:04 -0400

    count.sh v2; now generates mismatch report

--- count.sh
+++ count.sh
@@ -1,29 +1,63 @@
 #!/bin/bash
 
-DIR="$1"
-output="output.txt"
+SOURCE="$1"
+TARGET="$2"
+output="output_"$(date)".txt"
 
-if [ ! -d "$DIR" ]; then
+if [ ! -d "$SOURCE" ]; then
     echo "Not a directory"
     exit 1
 fi
 
-directories=()
-counts=()
-num_dirs=0
+if [ ! -d "$TARGET" ]; then
+    echo "Not a directory"
+    exit 1
+fi
 
-for sub_dir in "$DIR"/*/; do
+# source_dirs=()
+# target_dirs=()
+# num_dirs=0
+
+ne_dirs=()
+
+for sub_dir in "$SOURCE"/*/; do
     if [ -d "$sub_dir" ]; then
-        (( num_dirs++ ))
-        directories+=("$sub_dir")
+        folder="$(basename "$sub_dir")"
+        source_count=$(ls -1 "$sub_dir" | wc -l)
+        target_count=$(ls -1 ""$TARGET"/"$folder"/" | wc -l)
+
+        if [ $source_count -ne $target_count ]; then
+            ne_dirs+=("$folder")
+        fi
     fi
 done
 
-for dir in "${directories[@]}"; do
-    echo -n ""$(basename "$dir")": " >> "$output"
-    ls -1 "$dir" | wc -l >> "$output"
-    # echo
+for dirs in "${ne_dirs[@]}"; do
+    source_path=""$SOURCE"/"$dirs""
+    echo "##### "$dirs" #####" >> "$output"
+    for file in "$source_path"/*; do
+        name=$(basename "$file")
+        target_file=""$TARGET"/"$dirs"/"$name""
+        if [ ! -e "$target_file" ]; then
+            echo ""$target_file" does not exist" >> "$output"
+            # cp "$source_path"/"$name" "$target_file"
+        fi
+    done
+    echo >> "$output"
 done
+
+# for sub_dir in "$TARGET"/*/; do
+#     if [ -d "$sub_dir" ]; then
+#         # (( num_dirs++ ))
+#         target_dirs+=("$sub_dir")
+#     fi
+# done
+
+# for dir in "${directories[@]}"; do
+#     echo -n ""$(basename "$dir")": " >> "$output"
+#     ls -1 "$dir" | wc -l >> "$output"
+#     # echo
+# done
 
 # echo "$num_dirs"
 
